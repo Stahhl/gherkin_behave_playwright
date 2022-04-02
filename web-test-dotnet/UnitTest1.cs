6db29c254c1e47117dd016cc6e05b9f4eaa57a39
Arvid Ståhl Strömberg <arvostahl@gmail.com>
2022-04-02 16:22:56 +0200

    init

---
+++ web-test-dotnet/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace web_test_dotnet;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
